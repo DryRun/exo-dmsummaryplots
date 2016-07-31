a9d602f357e34efadea99d7a7787c167ae9f9e80
Tristan du Pree <tristan.dupree@cern.ch>
2016-07-31 15:41:00 +0200

    Added dummy files for mono-photon and mono-jet, added prelim file for Dijet, added directories/files for Relic&DD, and added plotscripts

---
+++ plotMM.py
@@ -0,0 +1,158 @@
+#########################################
+#########################################
+###                                   ###
+### Draw awesome MET+X Summary plots  ###
+###                                   ###
+### (c) MET+X Combo                   ###
+###                                   ###
+#########################################
+#########################################
+
+from ROOT import *
+
+################
+### Settings ###
+################
+
+Mediator  = "Axial"
+METXonly  = True
+
+#################
+### Analyses ####
+#################
+
+metx = ["monojet","monophoton","monoZ"]
+analyses = ["relic"]+metx
+if not METXonly: analyses = ["relic","dijet"]+metx
+
+tgraph    = {}
+color     = {}
+text      = {}
+filepath  = {}
+linestyle = {}
+
+############# 
+### Files ###
+#############
+
+if Mediator == "Vector":
+    filepath["dijet"]      = "Dijet/ScanMM/Dijet_MM_V_2016ICHEP_obs.dat"
+    filepath["monophoton"] = "Monophoton/ScanMM/monophoton_dummy.dat"
+    filepath["relic"]      = "Relic/relicContour_V_g25.root"
+    filepath["monojet"]    = "Monojet/ScanMM/monojet_dummy.dat"
+    filepath["monoZ"]      = "MonoZll/ScanMM/monoz_vector_gq0p25_cl95_2015.txt"
+elif Mediator == "Axial":
+    filepath["dijet"]      = "Dijet/ScanMM/Dijet_MM_A_2016ICHEP_obs.dat"
+    filepath["monophoton"] = "Monophoton/ScanMM/monophoton_dummy.dat"
+    filepath["relic"]      = "Relic/relicContour_A_g25.root"
+    filepath["monojet"]    = "Monojet/ScanMM/monojet_dummy.dat"
+    filepath["monoZ"]      = "MonoZll/ScanMM/monoz_axial_gq0p25_cl95_2015.txt"
+
+#####################
+### Plot settings ###
+#####################
+
+linestyle["relic"]      = kDotted
+linestyle["dijet"]      = kSolid
+linestyle["monoZ"]      = kSolid
+### dummies dashed
+linestyle["monojet"]    = kDashed
+linestyle["monophoton"] = kDashed
+
+color["monojet"]    = kRed
+color["monophoton"] = kOrange
+color["monoZ"]      = kYellow
+color["relic"]      = kGray
+color["chi"]        = kBlack
+color["dijet"]      = kBlue
+
+text["monojet"]    = "Mono-jet [DUMMY - EXO-16-037]"
+text["monoZ"]      = "Mono-Z [EXO-16-038]"
+text["monophoton"] = "Mono-\gamma  [DUMMY - EXO-16-039]"
+text["relic"]      = "\Omega_{c} x h^{2} \leq 0.12"
+text["chi"]        = "chi (exp.excl.)"
+text["dijet"]      = "Dijet [EXO-16-032]"
+
+####################
+### Get datfiles ###
+### Get graphs   ###
+####################
+
+### to do: make more generic for dat/root formats (if necessary)
+
+for analysis in analyses: 
+    if analysis == "relic":
+        mylist = TFile(filepath["relic"]).Get("mytlist")
+        print "==> Relic list length = ",mylist.GetSize()
+        tgraph["relic"] = mylist.At(0)
+    else :
+        tgraph[analysis] = TGraph(filepath[analysis])
+
+###################
+### Make Canvas ###
+### Set ranges  ###
+### Add legend  ###
+###################
+
+C=TCanvas("C","C",1000,600)
+C.cd(1)
+
+tgraph["relic"].SetTitle("CMS Preliminary - EXO DM Summary [ICHEP v1]")
+tgraph["relic"].GetXaxis().SetTitle("M_{Med} [GeV]")
+tgraph["relic"].GetYaxis().SetTitle("m_{DM} [GeV]")
+tgraph["relic"].GetXaxis().SetTitleOffset(1.0)
+tgraph["relic"].GetYaxis().SetTitleOffset(1.0)
+tgraph["relic"].GetXaxis().SetTitleSize(0.045)
+tgraph["relic"].GetYaxis().SetTitleSize(0.045)
+if METXonly: 
+    tgraph["relic"].GetXaxis().SetRangeUser(0,2000)
+    tgraph["relic"].GetYaxis().SetRangeUser(0, 700)
+else:        
+    tgraph["relic"].GetXaxis().SetRangeUser(0,4000)
+    tgraph["relic"].GetYaxis().SetRangeUser(0,1200)
+tgraph["relic"].Draw()
+
+if METXonly: leg=C.BuildLegend(0.15,0.60,0.40,0.88)
+else:        leg=C.BuildLegend(0.63,0.15,0.90,0.45)
+leg.SetBorderSize(0)
+leg.SetTextFont(42)
+leg.Clear()
+leg.SetHeader(Mediator+" (g_{q}=0.25, g_{DM}=1) obs.excl.95%CL")
+
+############
+### Draw ###
+############
+
+for analysis in analyses:
+    tgraph[analysis].SetLineColor(color[analysis])
+    tgraph[analysis].SetFillColor(color[analysis])
+    tgraph[analysis].SetFillStyle(3005)
+    tgraph[analysis].SetLineWidth( 404)
+    tgraph[analysis].SetLineStyle(linestyle[analysis])
+    if analysis == "relic":
+        for i in range(0,mylist.GetSize()):
+            tgraph["relic"] = mylist.At(i)
+            tgraph["relic"].SetLineColor(color[analysis])
+            tgraph["relic"].SetFillColor(color[analysis])
+            tgraph["relic"].SetLineStyle(linestyle[analysis])
+            tgraph["relic"].SetLineWidth(-404)
+            tgraph["relic"].SetFillStyle(3005)
+            tgraph["relic"].Draw("same")
+    elif analysis == "chi":
+        tgraph[analysis].SetLineWidth(-404)
+    leg.AddEntry(tgraph[analysis],text[analysis])
+    tgraph[analysis].Draw("same")
+
+leg.Draw()
+C.Update()
+
+############
+### Save ###
+############
+
+if METXonly: C.SaveAs(Mediator+"_METX_Summary_ICHEP.pdf")
+else       : C.SaveAs(Mediator+"_EXO_Summary_ICHEP.pdf")
+
+###########
+### FIN ###
+###########
---
+++ plotMx.py
@@ -0,0 +1,94 @@
+#########################################
+#########################################
+###                                   ###
+### Draw awesome MET+X Summary plots  ###
+###                                   ###
+### (c) MET+X Combo                   ###
+###                                   ###
+#########################################
+#########################################
+
+from ROOT import *
+
+#################
+### Analyses ####
+#################
+
+analyses = ["LUX","PandaX"]
+
+filename = {}
+tgraph   = {}
+text     = {}
+color    = {}
+
+####################
+### Get datfiles ###
+### Get graphs   ###
+####################
+
+filename["LUX"]    = "DD/LUX_SI_DMTools_Jul2016.dat"
+filename["PandaX"] = "DD/PandaX_SI_arxiv_Jul2016.dat"
+
+text["LUX"] = "LUX"
+text["PandaX"] = "PandaX"
+
+color["LUX"] = kBlue
+color["PandaX"] = kRed
+
+### to do: make more generic for dat/root formats
+
+for analysis in analyses: 
+    tgraph[analysis] = TGraph(filename[analysis])
+
+###################
+### Make Canvas ###
+### Set ranges  ###
+### Add legend  ###
+###################
+
+C=TCanvas("C","C",600,600)
+C.cd(1)
+C.cd(1).SetLogx()
+C.cd(1).SetLogy()
+
+tgraph["LUX"].SetTitle("DD Summer 2016")
+tgraph["LUX"].GetXaxis().SetTitle("m_{DM} [GeV]")
+tgraph["LUX"].GetYaxis().SetTitle("#sigma_{SI} [cm^{2}]")
+tgraph["LUX"].GetXaxis().SetTitleOffset(1.0)
+tgraph["LUX"].GetYaxis().SetTitleOffset(1.5)
+tgraph["LUX"].GetXaxis().SetTitleSize(0.045)
+tgraph["LUX"].GetYaxis().SetTitleSize(0.045)
+tgraph["LUX"].GetXaxis().SetRangeUser(0,1000)
+#tgraph[""].GetYaxis().SetRangeUser(0,1200)
+tgraph["LUX"].Draw()
+
+leg=C.BuildLegend(0.15,0.50,0.45,0.88)
+leg.SetBorderSize(0)
+leg.SetTextFont(42)
+leg.Clear()
+leg.SetHeader("#sigma_{SI}")
+
+############
+### Draw ###
+############
+
+for analysis in analyses:
+    tgraph[analysis].SetLineColor(color[analysis])
+    tgraph[analysis].SetFillColor(color[analysis])
+    tgraph[analysis].SetFillStyle(3005)
+    tgraph[analysis].SetLineWidth( 404)
+    if analysis == "LUX":
+        tgraph[analysis].SetFillColor(color[analysis])
+        tgraph[analysis].SetLineStyle(kDotted)
+        tgraph[analysis].SetLineWidth(404)
+        tgraph[analysis].SetFillStyle(3005)
+    elif analysis == "PandaX":
+        tgraph[analysis].SetLineWidth(-404)
+    leg.AddEntry(tgraph[analysis],text[analysis])
+    tgraph[analysis].Draw("same")
+
+leg.Draw()
+
+###########
+### FIN ###
+###########

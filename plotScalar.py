d7e6ca18db648089fe04e3c7f25b7694651dcc72
Marco <iesc1983@gmail.com>
2017-05-11 17:43:24 +0200

    modified plotScalar

--- plotScalar.py
+++ plotScalar.py
@@ -24,18 +24,21 @@ ObsOnly  = ast.literal_eval(raw_input('Obs only? [True or False]: '))
 
 analyses = ["BSM",
             "monojet_obs",
-            "DMtt_obs",
+            "monoz_obs",
+            #"DMtt_obs",
             #"METbb_DMtt_obs",
-            "METbb_DMbb_obs"
+            #"METbb_DMbb_obs"
             #,"METbb_DMhf_obs"
+            "METHF_DMhf_obs"
             ]
 
 if not ObsOnly: analyses=["BSM",
                           "monojet_obs",   "monojet_exp",
-                          "DMtt_obs",      "DMtt_exp",
-                          "METbb_DMbb_obs","METbb_DMbb_exp"
+                          "monoz_obs",     "monoz_exp",
+                          #"DMtt_obs",      "DMtt_exp",
+                          #"METbb_DMbb_obs","METbb_DMbb_exp"
                           #"METbb_DMtt_exp","METbb_DMtt_obs",
-                          #"METbb_DMhf_exp","METbb_DMhf_obs",
+                          "METHF_DMhf_obs","METHF_DMhf_exp",
                           ]
 
 tgraph    = {}
@@ -49,27 +52,35 @@ linestyle = {}
 #############
 
 if Mediator == "Scalar":
-    filepath["monojet_obs"]    = "Monojet/ScanMM/limit_1D_s_fermion.root"
-    filepath["monojet_exp"]    = "Monojet/ScanMM/limit_1D_s_fermion.root"
+    filepath["monojet_obs"]    = "Monojet/ScanMM/forTristan/limit_1D_s_fermion.root"
+    filepath["monojet_exp"]    = "Monojet/ScanMM/forTristan/limit_1D_s_fermion.root"
+    filepath["monoz_obs"]      = "MonoZll/ScanMM/monoz_contour_observed_limit_scalar.root"
+    filepath["monoz_exp"]      = "MonoZll/ScanMM/monoz_limit_S_exp.txt"
     filepath["DMtt_obs"]       = "DMtt/DMtt_Scalar_ICHEP2016_Mx_obs.txt"
     filepath["DMtt_exp"]       = "DMtt/DMtt_Scalar_ICHEP2016_Mx_exp.txt"
     filepath["METbb_DMtt_obs"] = "DMbb/2015/Limits_hfDMs.root"
     filepath["METbb_DMbb_obs"] = "DMbb/2015/Limits_hfDMs.root"
     filepath["METbb_DMhf_obs"] = "DMbb/2015/Limits_hfDMs.root"
+    filepath["METHF_DMhf_obs"] = "DMHF/2015/HF_spin0_limits.root"
     filepath["METbb_DMtt_exp"] = "DMbb/2015/Limits_hfDMs.root"
     filepath["METbb_DMbb_exp"] = "DMbb/2015/Limits_hfDMs.root"
     filepath["METbb_DMhf_exp"] = "DMbb/2015/Limits_hfDMs.root"
+    filepath["METHF_DMhf_exp"] = "DMHF/2015/HF_spin0_limits.root"
 if Mediator == "Pseudo":
-    filepath["monojet_obs"]    = "Monojet/ScanMM/limit_1D_ps.root"
-    filepath["monojet_exp"]    = "Monojet/ScanMM/limit_1D_ps.root"
+    filepath["monojet_obs"]    = "Monojet/ScanMM/forTristan/limit_1D_ps.root"
+    filepath["monojet_exp"]    = "Monojet/ScanMM/forTristan/limit_1D_ps.root"
+    filepath["monoz_obs"]      = "MonoZll/ScanMM/monoz_contour_observed_limit_pseudo.root"
+    filepath["monoz_exp"]      = "MonoZll/ScanMM/monoz_limit_P_exp.txt"
     filepath["DMtt_obs"]       = "DMtt/DMtt_Pseudo_ICHEP2016_Mx_obs.txt"
     filepath["DMtt_exp"]       = "DMtt/DMtt_Pseudo_ICHEP2016_Mx_exp.txt"
     filepath["METbb_DMtt_obs"] = "DMbb/2015/Limits_hfDMps.root"
     filepath["METbb_DMbb_obs"] = "DMbb/2015/Limits_hfDMps.root"
     filepath["METbb_DMhf_obs"] = "DMbb/2015/Limits_hfDMps.root"
+    filepath["METHF_DMhf_obs"] = "DMHF/2015/HF_spin0_limits.root"
     filepath["METbb_DMtt_exp"] = "DMbb/2015/Limits_hfDMps.root"
     filepath["METbb_DMbb_exp"] = "DMbb/2015/Limits_hfDMps.root"
     filepath["METbb_DMhf_exp"] = "DMbb/2015/Limits_hfDMps.root"
+    filepath["METHF_DMhf_exp"] = "DMHF/2015/HF_spin0_limits.root"
 
 #######################
 ### Plot linestyles ###
@@ -79,15 +90,19 @@ linestyle["BSM" ]           = kDotted
 #MET+jets
 linestyle["monojet_obs"]    = kSolid
 linestyle["monojet_exp"]    = kDashed
+linestyle["monoz_obs"]    = kSolid
+linestyle["monoz_exp"]    = kDashed
 # HF
 linestyle["DMtt_obs"]       = kSolid
 linestyle["METbb_DMtt_obs"] = kSolid
 linestyle["METbb_DMbb_obs"] = kSolid
 linestyle["METbb_DMhf_obs"] = kSolid
+linestyle["METHF_DMhf_obs"] = kSolid
 linestyle["DMtt_exp"]       = kDashed
 linestyle["METbb_DMtt_exp"] = kDashed
 linestyle["METbb_DMbb_exp"] = kDashed
 linestyle["METbb_DMhf_exp"] = kDashed
+linestyle["METHF_DMhf_exp"] = kDashed
 
 ###################
 ### Plot colors ###
@@ -95,35 +110,43 @@ linestyle["METbb_DMhf_exp"] = kDashed
 
 color["BSM"]            = kBlack
 color["monojet_obs"]    = kRed+1
+color["monoz_obs"]      = kOrange-3
 color["DMtt_obs"]       = kGreen+2
 color["METbb_DMbb_obs"] = kGreen+1
 ##
-color["METbb_DMhf_obs"] = kAzure+2
-color["METbb_DMtt_obs"] = kAzure+2
+color["METbb_DMhf_obs"] = kGreen+2
+color["METbb_DMtt_obs"] = kGreen+2
+color["METHF_DMhf_obs"] = kGreen+2
 ##
 color["monojet_exp"]    = color["monojet_obs"]
+color["monoz_exp"]    = color["monoz_obs"]
 color["DMtt_exp"]       = color["DMtt_obs"]
 color["METbb_DMbb_exp"] = color["METbb_DMbb_obs"]
 color["METbb_DMtt_exp"] = color["METbb_DMtt_obs"]
 color["METbb_DMhf_exp"] = color["METbb_DMhf_obs"]
+color["METHF_DMhf_exp"] = color["METHF_DMhf_obs"]
 
 ##################
 ### Plot texts ###
 ##################
 
 text["BSM"]            = "#sigma_{theory} (LHC DM WG)"
-if   Mediator == "Scalar": text["monojet_obs"] = "DM + j/V_{qq} (12.9 fb^{-1})"
-elif Mediator == "Pseudo": text["monojet_obs"] = "DM + j/V_{qq} (12.9 fb^{-1}) #it{EXO-16-037}"
+if   Mediator == "Scalar": text["monojet_obs"] = "DM + j (fermion only) #it{[EXO-16-048]}"
+elif Mediator == "Pseudo": text["monojet_obs"] = "DM + j/V_{qq} #it{[EXO-16-048]}"
+text["monoz_obs"]       = "DM + Z_{ll} #it{[EXO-16-052]}"
 text["DMtt_obs"]       = "DM + tt (2.2 fb^{-1}) #it{EXO-16-005}"
 text["METbb_DMbb_obs"] = "DM + bb (2.2 fb^{-1}) #it{B2G-15-007}"
 text["METbb_DMtt_obs"] = "DM + tt (nj<4) [B2G-15-007]"
 text["METbb_DMhf_obs"] = "MET+bb: DM+HF [B2G-15-007]"
+text["METHF_DMhf_obs"] = "MET+tt/bb: #it{EXO-16-005} #it{B2G-15-007}"
 #exp
 text["monojet_exp"]    = "exp.excl 95%CL"
+text["monoz_exp"]    = "exp.excl 95%CL"
 text["DMtt_exp"]       = "exp.excl 95%CL"
 text["METbb_DMbb_exp"] = "exp.excl 95%CL"
 text["METbb_DMtt_exp"] = "exp.excl 95%CL"
 text["METbb_DMhf_exp"] = "exp.excl 95%CL"
+text["METHF_DMhf_exp"] = "exp.excl 95%CL"
 
 text["gen_obs"] = "Observed exclusion 95% CL"
 text["gen_exp"] = "Expected exclusion 95% CL"
@@ -138,13 +161,24 @@ f1 = TF1("f1","1",0,1000)
 for analysis in analyses: 
     if   analysis == "BSM"            : tgraph[analysis] = TGraph(f1)
     elif analysis == "monojet_obs"    : tgraph[analysis] = TFile(filepath[analysis]).Get("obs")
+    elif analysis == "monoz_obs"    : tgraph[analysis] = TFile(filepath[analysis]).Get("obs")
     elif analysis == "monojet_exp"    : tgraph[analysis] = TFile(filepath[analysis]).Get("exp")
     elif analysis == "METbb_DMbb_obs" : tgraph[analysis] = TFile(filepath[analysis]).Get("bbObs0s")
     elif analysis == "METbb_DMtt_obs" : tgraph[analysis] = TFile(filepath[analysis]).Get("ttObs0s")
     elif analysis == "METbb_DMhf_obs" : tgraph[analysis] = TFile(filepath[analysis]).Get("hfObs0s")
+    elif analysis == "METHF_DMhf_obs" : 
+        if Mediator == "Scalar" :
+            tgraph[analysis] = TFile(filepath[analysis]).Get("obs_hf_scalar")
+        else :
+            tgraph[analysis] = TFile(filepath[analysis]).Get("obs_hf_pseudoscalar")
     elif analysis == "METbb_DMbb_exp" : tgraph[analysis] = TFile(filepath[analysis]).Get("bbExp0s")
     elif analysis == "METbb_DMtt_exp" : tgraph[analysis] = TFile(filepath[analysis]).Get("ttExp0s")
     elif analysis == "METbb_DMhf_exp" : tgraph[analysis] = TFile(filepath[analysis]).Get("hfExp0s")
+    elif analysis == "METHF_DMhf_exp" : 
+        if Mediator == "Scalar" :
+            tgraph[analysis] = TFile(filepath[analysis]).Get("exp_hf_scalar")
+        else :
+            tgraph[analysis] = TFile(filepath[analysis]).Get("exp_hf_pseudoscalar")
     else                              : tgraph[analysis] = TGraph(filepath[analysis])
 
 ###################
@@ -153,7 +187,7 @@ for analysis in analyses:
 ###################
 
 C=TCanvas("C","C",750,600)
-C.cd(1).SetLogx()
+#C.cd(1).SetLogx()
 C.cd(1).SetLogy()
 
 tgraph["monojet_obs"].SetTitle("")
@@ -164,7 +198,7 @@ tgraph["monojet_obs"].GetYaxis().SetTitleOffset(1.0)
 tgraph["monojet_obs"].GetXaxis().SetTitleSize(0.045)
 tgraph["monojet_obs"].GetYaxis().SetTitleSize(0.045)
 tgraph["monojet_obs"].GetXaxis().SetRangeUser(10,    500)
-tgraph["monojet_obs"].GetYaxis().SetRangeUser(0.1, 100000)
+tgraph["monojet_obs"].GetYaxis().SetRangeUser(0.1, 60)
 tgraph["monojet_obs"].Draw()
 
 ##################
@@ -189,7 +223,7 @@ tex1.Draw("same")
 tex2.Draw("same")
 tex3.Draw("same")
 
-if Mediator == "Scalar": leg=C.BuildLegend(0.21,0.59,0.55,0.88)
+if Mediator == "Scalar": leg=C.BuildLegend(0.15,0.59,0.50,0.88)
 if Mediator == "Pseudo": leg=C.BuildLegend(0.21,0.62,0.55,0.88)
 leg.SetBorderSize(0)
 leg.SetTextFont(42)
@@ -202,12 +236,12 @@ leg2.SetFillColor(0)
 leg2.Clear()
 leg2.SetHeader("#bf{CMS} #it{Preliminary}")
 
-leg3=C.BuildLegend(0.74,0.905,1.54,0.955)
+leg3=C.BuildLegend(0.44,0.905,1.54,0.955)
 leg3.SetBorderSize(0)
 leg3.SetTextFont(42)
 leg3.SetFillColor(0)
 leg3.Clear()
-leg3.SetHeader("#it{ICHEP 2016}")
+leg3.SetHeader("2.2 fb^{-1} & 35.9 fb^{-1} (13 TeV)")
 
 ############
 ### Draw ###
@@ -248,10 +282,12 @@ tgraph["fermion"].SetLineColor(kWhite)
 text["fermion"] = "fermion only #it{EXO-16-037}"
 
 if   Mediator == "Scalar":
-    for analysis in ["gen_obs","gen_exp","METbb_DMbb_obs","DMtt_obs","monojet_obs","fermion","BSM"]:
+#    for analysis in ["gen_obs","gen_exp","METbb_DMbb_obs","DMtt_obs","monojet_obs","fermion","BSM"]:
+    for analysis in ["gen_obs","gen_exp","METHF_DMhf_obs","METHF_DMhf_exp","monoz_obs","monoz_exp","monojet_obs","BSM"]:
         leg.AddEntry(tgraph[analysis],text[analysis])
 elif Mediator == "Pseudo":
-    for analysis in ["gen_obs","gen_exp","METbb_DMbb_obs","DMtt_obs","monojet_obs","BSM"]:
+#    for analysis in ["gen_obs","gen_exp","METbb_DMbb_obs","DMtt_obs","monojet_obs","BSM"]:
+    for analysis in ["gen_obs","gen_exp","METHF_DMhf_obs","METHF_DMhf_exp","monoz_obs","monoz_exp","monojet_obs","BSM"]:
         leg.AddEntry(tgraph[analysis],text[analysis])
 
 leg.Draw()
@@ -266,8 +302,8 @@ C.Update()
 ### Save ###
 ############
 
-if ObsOnly: C.SaveAs(Mediator+"_METX_Summary_ICHEP_obsonly.pdf")
-else      : C.SaveAs(Mediator+"_METX_Summary_ICHEP_obsnexp.pdf")
+if ObsOnly: C.SaveAs(Mediator+"_METX_Summary_obsonly.pdf")
+else      : C.SaveAs(Mediator+"_METX_Summary_obsnexp.pdf")
 
 ###########
 ### FIN ###
